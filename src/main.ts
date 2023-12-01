3c5c20e54dce0f7ef1d624a26eda56f573e8add1
dev-mohamed77 <developer.mohamed55@gmail.com>
2023-12-01 14:33:42 +0200

    Add messages Features with delete port

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ const port = 9876 || process.env.PORT;
 
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
-  app.enableCors();
+  // app.enableCors({
+  //   origin: ""
+  // });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   await app.listen(port);
