21f21e2173201567e527b3286e93aa8c16692ae6
dev-mohamed77 <developer.mohamed55@gmail.com>
2023-12-01 04:11:51 +0200

    Add messages Features with delete port

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 
-const port = process.env.PORT;
+const port = 9876 || process.env.PORT;
 
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
